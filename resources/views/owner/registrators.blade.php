be1b77faeb891e6411064b27010206b7e1ecc053
wira_sajaa <wirasanjaya142@gmail.com>
2023-01-25 16:02:03 +0700

    feat: sort registrators by status

--- resources/views/owner/registrators.blade.php
+++ resources/views/owner/registrators.blade.php
@@ -8,15 +8,13 @@
         <hr>
         <div class="filter-table my-4 d-flex justify-content-end">
           <form action="">
-            <select class="form-select" id="filter">
-              <option selected>filter</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+            <select class="form-select" id="filter" onchange="sortTable(this.value)">
+              <option>filter</option>
+              <option value="status">Status</option>
             </select>
           </form>
         </div>
-        <table class="table align-middle table-striped table-hover">
+        <table class="table align-middle table-striped table-hover" id="myTable">
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -64,4 +62,49 @@
       </div>
     </div>
   </div>
+@endsection
+@section('script')
+  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script>
+    function sortTable(value) {
+      if (value == "status") {
+        var table, rows, switching, i, x, y, shouldSwitch;
+        table = document.getElementById("myTable");
+        switching = true;
+        /*Make a loop that will continue until
+        no switching has been done:*/
+        while (switching) {
+          //start by saying: no switching is done:
+          switching = false;
+          rows = table.rows;
+          /*Loop through all table rows (except the
+          first, which contains table headers):*/
+          for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[5];
+            y = rows[i + 1].getElementsByTagName("TD")[5];
+            //check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              //if so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
+          if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+          }
+        }
+        rows = table.rows;
+        for (i = 1; i < (rows.length); i++) {
+          rows[i].getElementsByTagName("th")[0].innerHTML = i;
+        }
+      }
+    }
+  </script>
 @endsection
