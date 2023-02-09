5f1fb39f8270ff8eb4490a13923143f54d182bc9
wira_sajaa <wirasanjaya142@gmail.com>
2023-02-09 10:37:56 +0700

    version:0.1

--- resources/views/owner/forms.blade.php
+++ resources/views/owner/forms.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
   <div class="container">
-    <div class="row">
+    <div class="row gap-3">
       <div class="col-12">
         @if (session('success'))
           <div class="alert alert-success">
